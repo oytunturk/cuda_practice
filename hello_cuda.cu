eeabb2ec9b2f6e74310f09c54af4d64f0065c3ca
oytunturk <oytunturk@github.com>
2024-01-30 17:30:08 -0800

    First example functional on colab

--- hello_cuda.cu
+++ hello_cuda.cu
@@ -2,3 +2,19 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+
+__global__ void hello_world() {
+  printf("Hello CUDA\n");
+}
+
+int main() {
+  hello_world << <1, 1>> > ();
+
+  //Wait until kernel call is completed
+  cudaDeviceSynchronize();
+  
+  //Reset the device
+  cudaDeviceReset();
+
+  return 0;
+}
